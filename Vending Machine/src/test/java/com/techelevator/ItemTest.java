939dccd5fd7c372237afa45739bc03b76170d725
Shuva Sharma <shuvashissharma12@gmail.com>
2024-09-19 16:49:52 -0400

    new test

--- Vending Machine/src/test/java/com/techelevator/ItemTest.java
+++ Vending Machine/src/test/java/com/techelevator/ItemTest.java
@@ -6,14 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ItemTest {
 
-    @Test
+        @Test
     public void testItemCreation() {
-        Item item = new Item("A1", "Potato Crisps", 3.05, 5, "Chip");
-        assertEquals("Potato Crisps", item.getName());
+        Item item = new Item("A1", "Barbeque", 3.05, 5, "Chip");
+        assertEquals("Barbeque", item.getName());
         assertEquals(3.05, item.getPrice());
         assertEquals("A1", item.getSlotId());
         assertEquals("Chip", item.getType());
     }
+
 
 }
 
